93c6505e4c37dfc62b00e579a03dfb1d1e2accb5
StefanStenStark <110032501+StefanStenStark@users.noreply.github.com>
2024-09-06 10:01:19 +0200

    feat: Models done

--- GGbackend/Models/ActionCard.cs
+++ GGbackend/Models/ActionCard.cs
@@ -1,3 +1,7 @@
 public class ActionCard{
-    
+    public int Id {get; set;}
+    public string Type {get; set;}
+    public string Title {get; set;}
+    public string Instruction {get; set;}
+
 }
