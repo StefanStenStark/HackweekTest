aa7099d5ddb88e7f7df535e63901122fd9090c35
StefanStenStark <110032501+StefanStenStark@users.noreply.github.com>
2024-09-09 09:21:50 +0200

    feat: changed border and added text to card

--- GGFrontend/src/components/Cards.tsx
+++ GGFrontend/src/components/Cards.tsx
@@ -7,11 +7,13 @@ export default function Cards() {
   return (
     <>
       <div className="card">
+        <p>Wood</p>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
       </div>
       <div className="card">
+        <p>Stone</p>
         <button onClick={() => setCountTwo((countTwo) => countTwo + 1)}>
           count is {countTwo}
         </button>
